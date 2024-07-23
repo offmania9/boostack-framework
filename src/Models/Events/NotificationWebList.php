caf623cb1157578aa7d48fefb0e284609bcadb44
Stefano Spagnolo <>
2024-07-23 13:42:42 +0200

    update NotificationWebList

--- src/Models/Events/NotificationWebList.php
+++ src/Models/Events/NotificationWebList.php
@@ -4,6 +4,7 @@ namespace Boostack\Models\Events;
 
 use Boostack\Models\BaseList;
 use Boostack\Models\Events\NotificationWeb;
+use Boostack\Models\Session\Session;
 
 class NotificationWebList extends BaseList
 {
@@ -12,5 +13,20 @@ class NotificationWebList extends BaseList
     public function __construct()
     {
         parent::init();
+    }
+
+    public function loadMyPending($num_items = 100)
+    {
+        $filter = array();
+        $filter[] = array("status", "=", "pending");
+        $filter[] = array("id_user_to", "=", Session::getUserID());
+        $this->view($filter, "created_at", "desc", $num_items);
+    }
+
+    public function loadMy($num_items = 100)
+    {
+        $filter = array();
+        $filter[] = array("id_user_to", "=", Session::getUserID());
+        $this->view($filter, "created_at", "desc", $num_items);
     }
 }
